b3f1bd1c1975879c7381dac08624884cd07aeda7
jay3ss <mentoce@gmail.com>
2022-12-12 17:36:37 -0800

    Add function to read column names

--- pysqliteapi/database.py
+++ pysqliteapi/database.py
@@ -10,3 +10,12 @@ def read_table_names(db: sqlite3.Connection) -> List[str]:
     query = "SELECT name FROM sqlite_master WHERE type='table';"
     with db:
         return [name[0] for name in db.execute(query).fetchall()]
+
+
+def read_column_names(db: sqlite3.Connection, table: str) -> List[str]:
+    """
+    Returns the column names from the given table and database
+    """
+    query = "PRAGMA table_info({});".format(table)
+    with db:
+        return [col[1] for col in db.execute(query).fetchall()]
