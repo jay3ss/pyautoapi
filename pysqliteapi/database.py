ee52631b3c00add899b4e289d1305fd765bae582
jay ess <mentoce@gmail.com>
2022-12-12 22:23:04 -0800

    Add docstring documentation

--- pysqliteapi/database.py
+++ pysqliteapi/database.py
@@ -2,8 +2,13 @@ import sqlite3
 from typing import List
 
 def read_table_names(db: sqlite3.Connection) -> List[str]:
-    """
-    Returns the table names of the given database as a list of strings
+    """Returns the table names of the given database as a list of strings
+
+    Args:
+        db (sqlite3.Connection): connection to the database
+
+    Returns:
+        List[str]: table names
     """
     # adapted from:
     # https://stackoverflow.com/a/34570549
@@ -13,17 +18,29 @@ def read_table_names(db: sqlite3.Connection) -> List[str]:
 
 
 def read_column_names(db: sqlite3.Connection, table: str) -> List[str]:
-    """
-    Returns the column names from the given table and database
+    """Returns the column names from the given table and database
+
+    Args:
+        db (sqlite3.Connection): connection to the database
+        table (str): table name
+
+    Returns:
+        List[str]: column names
     """
     query = "PRAGMA table_info({});".format(table)
     with db:
         return [col[1] for col in db.execute(query).fetchall()]
 
 
-def read_column_names_and_types(db: sqlite3.Connection, table: str) -> List[dict]:
-    """
-    Returns a dict of column names and column types as a key-value pair
+def read_column_names_and_types(db: sqlite3.Connection, table: str) -> dict:
+    """Returns a dict of column names and column types as a key-value pair
+
+    Args:
+        db (sqlite3.Connection): connection to the database
+        table (str): table name
+
+    Returns:
+        dict: column names and types as key-value pairs
     """
     type_map = {
         "INTEGER": int,
