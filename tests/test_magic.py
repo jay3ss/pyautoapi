1d42419397a293cd1f263b3cede8a1540768029d
jay ess <mentoce@gmail.com>
2022-12-14 20:15:30 -0800

    Add test for magic module

---
+++ tests/test_magic.py
@@ -0,0 +1,18 @@
+import types
+
+from pysqliteapi import magic
+
+
+def test_compiling_function():
+    function_definition = """
+    def foo(x, y=2):
+        z = x*y + 3
+        return z**2
+    """
+    func = magic.compile_function(function_definition)
+    assert type(func) == types.FunctionType
+    assert func(2, 3) == 81
+
+
+if __name__ == "__main__":
+    test_compiling_function()
