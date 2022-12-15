6092a3fdc352df2005b8fbedd63737c3529881d5
jay ess <mentoce@gmail.com>
2022-12-14 20:34:54 -0800

    Add tests for type annotations and defaults of compiled function

--- tests/test_magic.py
+++ tests/test_magic.py
@@ -12,3 +12,22 @@ def foo(x, y=2):
     func = magic.compile_function(function_definition)
     assert type(func) == types.FunctionType
     assert func(2, 3) == 81
+
+
+def test_type_annotations_of_compiled_function():
+    function_definition = """
+def foo(x: int, y: float):
+    z = x*y + 3
+    return z**2
+    """
+    func = magic.compile_function(function_definition)
+    assert func.__annotations__ == {"x": int, "y": float}
+
+def test_type_defaults_of_compiled_function():
+    function_definition = """
+def foo(x: int = 13, y: float = 0):
+    z = x*y + 3
+    return z**2
+    """
+    func = magic.compile_function(function_definition)
+    assert func.__defaults__ == (13, 0)
