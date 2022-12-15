b837bd0c833a9470fd773e8f382494d4dd362c07
jay ess <mentoce@gmail.com>
2022-12-14 20:22:00 -0800

    Remove line from debugging

--- tests/test_magic.py
+++ tests/test_magic.py
@@ -12,7 +12,3 @@ def foo(x, y=2):
     func = magic.compile_function(function_definition)
     assert type(func) == types.FunctionType
     assert func(2, 3) == 81
-
-
-if __name__ == "__main__":
-    test_compiling_function()
