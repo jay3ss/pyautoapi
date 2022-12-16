3825b29c5ed3343e4f47f56d3605052dd5fa95ca
jay ess <mentoce@gmail.com>
2022-12-16 13:36:09 -0800

    Remove functions that accomplish same things as inspect()

--- pysqliteapi/database.py
+++ pysqliteapi/database.py
@@ -23,60 +23,6 @@ def load_db(path: str|PathLike) -> Engine:
     return sa.create_engine(f"sqlite:///{path}", echo=True)
 
 
-def read_table_names(db: Engine) -> List[str]:
-    """Returns the table names of the given database as a list of strings
-
-    Args:
-        db (Engine): connection to the database
-
-    Returns:
-        List[str]: table names
-    """
-    # adapted from:
-    # https://stackoverflow.com/a/34570549
-    query = "SELECT name FROM sqlite_master WHERE type='table';"
-    with db:
-        return [name[0] for name in db.execute(query).fetchall()]
-
-
-def read_column_names(db: Engine, table: str) -> List[str]:
-    """Returns the column names from the given table and database
-
-    Args:
-        db (Engine): connection to the database
-        table (str): table name
-
-    Returns:
-        List[str]: column names
-    """
-    query = "PRAGMA table_info({});".format(table)
-    with db:
-        return [col[1] for col in db.execute(query).fetchall()]
-
-
-def read_column_names_and_types(db: Engine, table: str) -> dict:
-    """Returns a dict of column names and column types as a key-value pair
-
-    Args:
-        db (Engine): connection to the database
-        table (str): table name
-
-    Returns:
-        dict: column names and types as key-value pairs
-    """
-    type_map = {
-        "INTEGER": int,
-        "NULL": None,
-        "REAL": float,
-        "NUMERIC": float,
-        "TEXT": str,
-        "BLOB": bytes,
-    }
-    query = "PRAGMA table_info({});".format(table)
-    with db:
-        return {col[1]: type_map[col[2]] for col in db.execute(query).fetchall()}
-
-
 def inspect(db: Engine) -> dict:
     """Inspects the given database and returns a dict of information about each
     table in the database. Each key in the dict contains a list of dicts with
