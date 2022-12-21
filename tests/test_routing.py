15a9987c78cb097aa8e165f9210e449592e53e9c
jay ess <mentoce@gmail.com>
2022-12-21 01:13:29 -0800

    Add tests for routing module

--- tests/test_routing.py
+++ tests/test_routing.py
@@ -1,1 +1,12 @@
 import pysqliteapi.routing as rt
+
+
+def test_create_endpoint_name():
+    path = "/this/is/my/path"
+    methods = ["GET", "POST"]
+    assert rt.create_endpoint_name(path, methods) == "get_post_this_is_my_path"
+    methods = ["get", "post"]
+    assert rt.create_endpoint_name(path, methods) == "get_post_this_is_my_path"
+    path = "this/is/my/path/"
+    assert rt.create_endpoint_name(path, methods) == "get_post_this_is_my_path"
+    path = "/this/is/my/path/"
