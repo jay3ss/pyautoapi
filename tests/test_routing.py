3a6feb19378fb7d793d1adaef12c6dd349dd7181
jay ess <mentoce@gmail.com>
2022-12-21 18:48:02 -0800

    Update test to match new API

--- tests/test_routing.py
+++ tests/test_routing.py
@@ -13,11 +13,11 @@ def test_create_endpoint_name():
 
 
 def test_create_path_name():
-    assert rt.create_path_name("this", "is", "my", "path") == "/this/is/my/path"
-    assert rt.create_path_name("") == "/"
-    assert rt.create_path_name(" ") == "/"
-    assert rt.create_path_name("this", "is", "my?", "path") == "/this/is/my/path"
-    assert rt.create_path_name("this", "is", "&!!!!", "path") == "/this/is/path"
-    assert rt.create_path_name("this", "IS", "my", "path") == "/this/is/my/path"
-    assert rt.create_path_name("this", "is", "my-path") == "/this/is/my-path"
-    assert rt.create_path_name("this", "is", "my_path") == "/this/is/my_path"
+    assert rt.create_path_name(["this", "is", "my", "path"]) == "/this/is/my/path"
+    assert rt.create_path_name([""]) == "/"
+    assert rt.create_path_name([" "]) == "/"
+    assert rt.create_path_name(["this", "is", "my?", "path"]) == "/this/is/my/path"
+    assert rt.create_path_name(["this", "is", "&!!!!", "path"]) == "/this/is/path"
+    assert rt.create_path_name(["this", "IS", "my", "path"]) == "/this/is/my/path"
+    assert rt.create_path_name(["this", "is", "my-path"]) == "/this/is/my-path"
+    assert rt.create_path_name(["this", "is", "my_path"]) == "/this/is/my_path"
