e5cd6e5afd9d18522402e5ab574181aaa0ad91e5
jay ess <mentoce@gmail.com>
2023-05-20 16:09:26 -0700

    Use physical db instead of in-memory for example

--- examples/example.py
+++ examples/example.py
@@ -1,11 +1,14 @@
+import pathlib
+
 import uvicorn
 
 import pyautoapi as pyapi
 from create_db import create_test_database
 
 
-test_db = ":memory:"
-create_test_database(test_db)
+test_db = "test.db"
+if not pathlib.Path(test_db).exists():
+    create_test_database(test_db)
 
 api = pyapi.PyAutoAPI(test_db)
 uvicorn.run(api, port=5555)
