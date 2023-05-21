c0edabd9c35900d9cb7bab9b7bcb92d6b48a0835
jay ess <mentoce@gmail.com>
2023-05-21 00:15:50 -0700

    Remove (comment out for now) QueryValidator, Query.read -> Query.execute, remove other Query methods since execute will do heavy lifting

--- pyautoapi/database.py
+++ pyautoapi/database.py
@@ -106,71 +106,71 @@ class Models:
         return self._models[key]
 
 
-class QueryValidator:
-    """Very basic SQLite query validator."""
+# class QueryValidator:
+#     """Very basic SQLite query validator."""
 
-    def __init__(self, engine: Engine) -> None:
-        self._engine = engine
-        self._info = inspect(engine)
+#     def __init__(self, engine: Engine) -> None:
+#         self._engine = engine
+#         self._info = inspect(engine)
 
-    def validate_params(
-        self,
-        table: str,
-        column: str = None,
-        conditional: str = None,
-        value: ValueTypes = None,
-        ignore_type: bool = True,
-    ) -> bool:
-        """Validate the params for the SQLite query
+#     def validate_params(
+#         self,
+#         table: str,
+#         column: str = None,
+#         conditional: str = None,
+#         value: ValueTypes = None,
+#         ignore_type: bool = True,
+#     ) -> bool:
+#         """Validate the params for the SQLite query
 
-        Args:
-            params (Iterable): the params to sanitize
+#         Args:
+#             params (Iterable): the params to sanitize
 
-        Returns:
-            bool: True if params are valid, False otherwise
-        """
-        is_valid = self._validate_query_params(table, column, conditional, value)
-        if not ignore_type:
-            is_valid = is_valid and self._validate_value_type(None)
+#         Returns:
+#             bool: True if params are valid, False otherwise
+#         """
+#         is_valid = self._validate_query_params(table, column, conditional, value)
+#         if not ignore_type:
+#             is_valid = is_valid and self._validate_value_type(None)
 
-        return is_valid
+#         return is_valid
 
-    def _validate_table(self, table: str) -> bool:
-        table_names = list(self._info)
-        return table in table_names
+#     def _validate_table(self, table: str) -> bool:
+#         table_names = list(self._info)
+#         return table in table_names
 
-    def _validate_column(self, table: str, column: str) -> bool:
-        column_names = [col["name"] for col in self._info[table]]
-        return column in column_names
+#     def _validate_column(self, table: str, column: str) -> bool:
+#         column_names = [col["name"] for col in self._info[table]]
+#         return column in column_names
 
-    def _validate_conditional(self, conditional: str) -> bool:
-        return conditional in ["=", "<", ">", "<>", "<=", ">=", "!="]
+#     def _validate_conditional(self, conditional: str) -> bool:
+#         return conditional in ["=", "<", ">", "<>", "<=", ">=", "!="]
 
-    def _validate_query_params(
-        self,
-        table: str,
-        column: str = None,
-        conditional: str = None,
-        value: ValueTypes = None,
-    ) -> bool:
-        if not self._validate_table(table):
-            return False
+#     def _validate_query_params(
+#         self,
+#         table: str,
+#         column: str = None,
+#         conditional: str = None,
+#         value: ValueTypes = None,
+#     ) -> bool:
+#         if not self._validate_table(table):
+#             return False
 
-        if conditional or value or column:
-            # if one of the above is present, then all need to be present
-            if not (conditional and value and column):
-                return False
+#         if conditional or value or column:
+#             # if one of the above is present, then all need to be present
+#             if not (conditional and value and column):
+#                 return False
 
-            if not self._validate_column(table, column):
-                return False
+#             if not self._validate_column(table, column):
+#                 return False
 
-            if not self._validate_conditional(conditional):
-                return False
+#             if not self._validate_conditional(conditional):
+#                 return False
 
-        return True
+#         return True
 
-    def _validate_value_type(self, type_: ValueTypes) -> bool:
-        raise NotImplementedError()
+#     def _validate_value_type(self, type_: ValueTypes) -> bool:
+#         raise NotImplementedError()
 
 
 class Query:
@@ -182,25 +182,12 @@ class Query:
     def __init__(self, engine: Engine) -> None:
         self._engine = engine
         self._models = Models(engine)
-        self._validator = QueryValidator(engine)
 
-    def create(self):
-        raise NotImplementedError()
-
-    def read(
-        self,
-        table: str,
-        column: str = None,
-        conditional: str = None,
-        value: ValueTypes = None,
-    ) -> List:
-        """Runs a select statement on the given table based on the given
-        conditions (i.e., puts the R in CRUD). If you want all data points,
-        then only set the `table` argument. Otherwise, you'll need to use all
-        arguments.
+    def execute(self, query: str) -> List:
+        """Runs the given query on the database
 
         Args:
-            table (str): the name of the table
+            query (str): the SQL query to execute
             col (str): (optional) the column to
             conditional (str): (optional) the comparison operator to be used
             from this set of values: ["=", "<", ">", "<>", "<=", ">="]
@@ -210,27 +197,11 @@ class Query:
         Returns:
             Lists: returns a read query
         """
-
-        if not self._validator.validate_params(table, column, conditional, value):
-            raise InvalidQueryError("Invalid query.")
-
-        Model = self._models[table]
         with Session(self._engine) as session:
-            if column and conditional and value:
-                if isinstance(value, str):
-                    value = f"'{value}'"
-                condition = sa.text(f"{getattr(Model, column)} {conditional} {value}")
-                statement = sa.select(Model).where(condition)
-            else:
-                statement = sa.select(Model)
+            statement = sa.text(query)
+            results = session.execute(statement).mappings().all()
 
-        return [res[0] for res in session.execute(statement).all()]
-
-    def update(self):
-        raise NotImplementedError()
-
-    def delete(self):
-        raise NotImplementedError()
+        return results
 
 
 class InvalidQueryError(Exception):
