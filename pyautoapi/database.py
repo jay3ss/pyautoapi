ef033bccdc16392bb530b9f3b3b680911268938e
jay ess <mentoce@gmail.com>
2023-05-21 00:48:04 -0700

    Remove lots of unused/commented-out code

--- pyautoapi/database.py
+++ pyautoapi/database.py
@@ -1,10 +1,9 @@
 import pathlib
+import sqlite3
 from typing import List, TypeVar
 
 import sqlalchemy as sa
 from sqlalchemy.engine.base import Engine
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm.decl_api import DeclarativeMeta
 from sqlalchemy.orm import Session
 
 
@@ -28,153 +27,6 @@ def load_db(path: str | PathLike, echo: bool = False) -> Engine:
     return sa.create_engine(f"sqlite:///{path}", echo=echo)
 
 
-def inspect(db: Engine) -> dict:
-    """Inspects the given database and returns a dict of information about each
-    table in the database. Each key in the dict contains a list of dicts with
-    information about each column in the table such as its name, type, and if
-    it's a primary key or not.
-
-    Args:
-        db (Engine): connection to the database
-
-    Returns:
-        dict: top-level keys are the table names with the values as list of dicts
-        of information of each column in the table. E.g.,
-
-            {
-                "table1": [
-                    {
-                        "name": "id",
-                        "type": Integer,
-                        "primary_key": True,
-                    },
-                    ...
-                ],
-                "table2": [...],
-                ...
-            }
-    """
-    type_map = {
-        "INTEGER": sa.Integer,
-        "NULL": None,
-        "REAL": sa.Float,
-        "NUMERIC": sa.Float,
-        "TEXT": sa.Text,
-        "BLOB": sa.LargeBinary,
-    }
-    insp = sa.inspect(db)
-    columns = {
-        table: [
-            {
-                "name": col["name"],
-                "type": type_map[str(col["type"])],
-                "primary_key": bool(col["primary_key"]),
-            }
-            for col in insp.get_columns(table)
-        ]
-        for table in insp.get_table_names()
-    }
-    return columns
-
-
-class Models:
-    def __init__(self, engine: Engine) -> None:
-        self._models = self._generate_models(engine)
-
-    def _generate_models(self, engine: Engine) -> list:
-        """Generates the models for each table in the given database
-
-        Returns:
-            list: list of SQLAlchemy models
-        """
-        insp = inspect(engine)
-        table_names = list(insp.keys())
-        Base = automap_base()
-        Base.prepare(autoload_with=engine, reflect=True)
-
-        return {
-            table_name: getattr(Base.classes, table_name) for table_name in table_names
-        }
-
-    @property
-    def models(self) -> dict:
-        return self._models
-
-    def __iter__(self) -> DeclarativeMeta:
-        for model in self._models.values():
-            yield model
-
-    def __getitem__(self, key):
-        return self._models[key]
-
-
-# class QueryValidator:
-#     """Very basic SQLite query validator."""
-
-#     def __init__(self, engine: Engine) -> None:
-#         self._engine = engine
-#         self._info = inspect(engine)
-
-#     def validate_params(
-#         self,
-#         table: str,
-#         column: str = None,
-#         conditional: str = None,
-#         value: ValueTypes = None,
-#         ignore_type: bool = True,
-#     ) -> bool:
-#         """Validate the params for the SQLite query
-
-#         Args:
-#             params (Iterable): the params to sanitize
-
-#         Returns:
-#             bool: True if params are valid, False otherwise
-#         """
-#         is_valid = self._validate_query_params(table, column, conditional, value)
-#         if not ignore_type:
-#             is_valid = is_valid and self._validate_value_type(None)
-
-#         return is_valid
-
-#     def _validate_table(self, table: str) -> bool:
-#         table_names = list(self._info)
-#         return table in table_names
-
-#     def _validate_column(self, table: str, column: str) -> bool:
-#         column_names = [col["name"] for col in self._info[table]]
-#         return column in column_names
-
-#     def _validate_conditional(self, conditional: str) -> bool:
-#         return conditional in ["=", "<", ">", "<>", "<=", ">=", "!="]
-
-#     def _validate_query_params(
-#         self,
-#         table: str,
-#         column: str = None,
-#         conditional: str = None,
-#         value: ValueTypes = None,
-#     ) -> bool:
-#         if not self._validate_table(table):
-#             return False
-
-#         if conditional or value or column:
-#             # if one of the above is present, then all need to be present
-#             if not (conditional and value and column):
-#                 return False
-
-#             if not self._validate_column(table, column):
-#                 return False
-
-#             if not self._validate_conditional(conditional):
-#                 return False
-
-#         return True
-
-#     def _validate_value_type(self, type_: ValueTypes) -> bool:
-#         raise NotImplementedError()
-
-
 class Query:
     """Object to query the database.
 
@@ -183,7 +35,6 @@ class Query:
 
     def __init__(self, engine: Engine) -> None:
         self._engine = engine
-        self._models = Models(engine)
 
     def execute(self, query: str) -> List:
         """Runs the given query on the database
@@ -196,6 +47,12 @@ class Query:
         """
         with Session(self._engine) as session:
             statement = sa.text(query)
-            results = session.execute(statement).mappings().all()
+            try:
+                results = session.execute(statement).mappings().all()
+            except sqlite3.OperationalError as e:
+                # TODO: implement logging
+                # for now, just print the error.
+                print(e, flush=True)
+
 
         return results
