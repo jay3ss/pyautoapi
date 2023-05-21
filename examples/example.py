d47a1a26067511038d9f7818f679e3ec3de45803
jay ess <mentoce@gmail.com>
2023-05-20 23:57:21 -0700

    Refactor example so that it can be run from the root of the project from the terminal

--- examples/example.py
+++ examples/example.py
@@ -1,9 +1,18 @@
+"""
+Example script. To run it, from the root of the directory run
+
+uvicorn examples.example:api --port <port>
+
+where <port> is your desired port. If you want to run it on the default port
+(8000) then omit the port flag.
+"""
+
 import pathlib
 
 import uvicorn
 
 import pyautoapi as pyapi
-from create_db import create_test_database
+from .create_db import create_test_database
 
 
 test_db = "test.db"
@@ -11,4 +20,7 @@ if not pathlib.Path(test_db).exists():
     create_test_database(test_db)
 
 api = pyapi.PyAutoAPI(test_db)
-uvicorn.run(api, port=5555)
+
+
+if __name__ == "__main__":
+    uvicorn.run(f"{pathlib.Path(__file__).stem}:api", port=5555, reload=True)
