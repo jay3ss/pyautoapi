485264c9041272d5ab7d8707d79f5fd31b39335d
jay ess <mentoce@gmail.com>
2023-05-23 00:58:28 -0700

    Bump up version to 0.0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PyAutoAPI",
-    version="0.0.2",
+    version="0.0.3",
     description="Easily and automatically turn a database into an API",
     author="jay3ss",
     long_description=long_description,
