d80557808539e392345a9d602dfdaa57bd8ea433
jay3ss <mentoce@gmail.com>
2023-05-22 17:15:57 -0700

    Rearrange SQL scripts to: one to create the tables, one to populate tables with data

---
+++ tests/data/create_tables.sql
@@ -0,0 +1,16 @@
+CREATE TABLE table1(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    txt TEXT,
+    num NUMERIC,
+    int INTEGER,
+    rl  REAL,
+    blb BLOB
+);
+CREATE TABLE table2(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    txt TEXT,
+    num NUMERIC,
+    int INTEGER,
+    rl  REAL,
+    blb BLOB
+);
